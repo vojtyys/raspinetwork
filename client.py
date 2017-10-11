be49fcba92cd2307572f74f5be1d5484b452d103
Vojtěch Pail <vojta.pail@seznam.cz>
2017-10-12 00:47:03 +0200

    Klient pracuje s verejnou adresou

--- client.py
+++ client.py
@@ -4,11 +4,11 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = 12345
-host = '192.168.0.200' 
+host = '89.177.139.187' 
 
 s.connect((host, port))
 print ('client: connected')
 print ('client: '+s.recv(4096).decode()) 
-msg = input('Client: Enter a message:')
+msg = input('Client: Enter a message: ')
 s.send(msg.encode())
 s.close()
