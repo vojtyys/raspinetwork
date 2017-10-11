4e54be1bd25f00c814937f11f03d796d247fefb8
Vojtech Pail <vojta.pail@seznam.cz>
2017-10-11 23:41:52 +0200

    Opravena inicializace socketu, zavorky u printu a cteni msg

--- client.py
+++ client.py
@@ -2,11 +2,12 @@
 
 import socket
 
-s = socket.socket(AF_INET,SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = 12345
 host = '89.177.139.187'
 
 s.connect((host, port))
-print s.recv(4096)
+print (s.recv(4096))
+msg = raw_input('Enter a message:')
 s.send(msg)
 s.close()
