2bea1738a6605de2f6f9657fdd06e43e9ed1caf4
Vojtech Pail <vojta.pail@seznam.cz>
2017-10-11 23:08:42 +0200

    Vytvoren server

---
+++ server.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import socket
+
+s = socket.socket()
+host = socket.gethostname()
+port = 12345
+s.bind((host, port)
+s.listen(5)
+
+while True:
+    c, addr = s.accept()
+	print ('Got connection from', addr)
+	c.send('Connected to',host,'.')
+	msg = s.recv(4096)
+	print (msg)
+	s.close
