7b70a5c5351293e92bb28d77afcae9e3e32ac272
Vojtech Pail <vojta.pail@seznam.cz>
2017-10-13 15:05:30 +0200

    Server používá thready může obsluhovat více požadavků najednou

--- server.py
+++ server.py
@@ -2,19 +2,35 @@
 # raspinetwork (C) 2017 Vojtech Pail https://github.com/vojtyys/raspinetwork/blob/master/LICENSE
 
 import socket
+import threading
+
+class serverThread(threading.Thread):
+    def __init__(self, ID, client, addr):
+        threading.Thread.__init__(self)
+        self.ID = ID
+        self.client = client
+        self.addr = addr
+
+    def run(self):
+        print('Server: Got connection from: ',self.addr)
+        self.client.send(("You are connected to:" + socket.gethostname() + '.').encode())
+        msg = self.client.recv(4096).decode()
+        print('Server: Msg from client ', self.addr, ': ', msg, '.')
+        self.client.send(msg.encode())
+        self.client.close()
+        
+    
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = ''
 port = 12345
 s.bind((host, port))
 s.listen(5)
-
+th = []
+index = 0
 while True:
     c, addr = s.accept()
-    print ('Server: Got connection from', addr)
-    c.send(('Connected to '+socket.gethostname()+'.').encode())
-    msg = c.recv(4096).decode();
-    print ('Server: '+msg)
-    c.send(msg.encode())
-    s.close
-    print('Server: Connection to ', addr, ' has been closed.')
+    t = serverThread(index, c, addr)
+    t.run()
+    th.append(t)
+s.close()
