458cbe0d5b402a1ca5436fc03d3136061f250fed
Vojtěch Pail <vojta.pail@seznam.cz>
2017-10-13 14:26:24 +0200

    Server is now multithreaded, can accept more connection at once.

--- server.py
+++ server.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
 
 import socket
+import threading
+
+class serverThread(threading.Thread):
+    def __init__(self, ID, client, addr):
+        threading.Thread.__init__(self)
+        self.ID = ID
+        self.client = client
+        self.addr = addr
+
+    def run(self):
+        print('Server: Got connection from: ', addr)
+        client.send(("You are connected to:" + socket.gethostname() + '.').encode())
+        print('Server: Msg from client ' + addr + ': ' + client.recv(4096).decode() + '.')
+        
+        
+    
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = ''
 port = 12345
 s.bind((host, port))
 s.listen(5)
-
+threads = []
+index = 0
 while True:
     c, addr = s.accept()
-    print ('Server: Got connection from', addr)
-    c.send(('Connected to '+socket.gethostname()+'.').encode())
-    msg = c.recv(4096).decode();
-    print ('Server: '+msg)
-    s.close
+    t = serverThread(index, c, addr)
+    s.close()
+   
